5dc9bc9eb7fa61ccf77a56a74ccc191bcf07b717
paing123 <paingpaing4228@gmail.com>
2019-11-29 20:48:47 +0630

    add Spring AOP

---
+++ tiktopgymmanagement/src/main/java/com/tiktop/config/MemberAccessObject.java
@@ -0,0 +1,19 @@
+package com.tiktop.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MemberAccessObject {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Before("execution(* com.tiktop.services.*.*(..))")
+	public void before(JoinPoint joinPoint) {
+		logger.info(" Check for user access ");
+		logger.info(" Allowed execution for {}", joinPoint);
+	}
+}
