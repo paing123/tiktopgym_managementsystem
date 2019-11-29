ea3b2d4430a4d5574ba90ab05759bf539fe0c845
paing123 <paingpaing4228@gmail.com>
2019-11-29 22:22:24 +0630

    update spring aop

--- tiktopgymmanagement/src/main/java/com/tiktop/config/MemberAccessObject.java
+++ tiktopgymmanagement/src/main/java/com/tiktop/config/MemberAccessObject.java
@@ -6,14 +6,19 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Aspect
 @Configuration
 public class MemberAccessObject {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Before("execution(* com.tiktop.services.*.*(..))")
-	public void before(JoinPoint joinPoint) {
-		logger.info(" Check for user access ");
-		logger.info(" Allowed execution for {}", joinPoint);
+	public void afterReturning(JoinPoint joinPoint) {
+		logger.info("Check for user access ");
+		logger.info("Allowed execution for {}",joinPoint);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		logger.info("Allowed execution {}",auth);
 	}
 }
