ca335364cfb0ed37fb0be5ff98dac7637797e7a6
paing123 <paingpaing4228@gmail.com>
2019-12-15 16:55:06 +0630

    update REST API for each member

--- tiktopgymmanagement/src/main/java/com/tiktop/controller/MemberRestController.java
+++ tiktopgymmanagement/src/main/java/com/tiktop/controller/MemberRestController.java
@@ -3,6 +3,7 @@ package com.tiktop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,13 @@ public class MemberRestController {
 	public List<Member> getAllMembers() {
 		List<Member> members = memberService.findMember(new Member());
 		return members;
+	}
+	
+	@RequestMapping(value = "/memberlist/{id}")
+	public Member getMember(@ModelAttribute("id") Integer id) {
+		Member member = new Member();
+		member.setMemberId(id);
+		List<Member> members = memberService.findMember(member);
+		return members.get(0);
 	}
 }
